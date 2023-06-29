b01977426036579abeec46816c4e6452bbe57936
Sean Fang <fangzhengonly@gmail.com>
2023-06-29 19:44:32 -0400

    add some other configurations

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const color: Color = {
+  red: 0,
+  blue: 1,
+  green: 1,
+};
+
+console.log(color);
+
+export default color;
