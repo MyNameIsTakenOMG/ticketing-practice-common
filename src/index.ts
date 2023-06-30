e3986299dd194457758750a6c137fa2c6b30755a
Sean Fang <fangzhengonly@gmail.com>
2023-06-30 15:05:46 -0400

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-// export * from './errors/bad-request-error';
-// export * from './errors/custom-error';
-// export * from './errors/database-connection-error';
-// export * from './errors/not-authorized-error';
-// export * from './errors/not-found-error';
-// export * from './errors/request-validation-error';
-// export * from './middleware/current-user';
-// export * from './middleware/error-handler';
-// export * from './middleware/require-auth';
-// export * from './middleware/validate-request';
-
+// re-export custom errors and middlewares
 export * from './errors/bad-request-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection-error';
